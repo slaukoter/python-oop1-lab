5b1eaf1f23fb6b0b8cf21a6992125fb6c1b91dd2
slaukoter <slaukoter@gmail.com>
2025-12-01 14:31:51 -0600

    passes tests

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,38 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count=0):
+        self.title = title
+        self._author = None
+        self._page_count = page_count
+        self._genre = None
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, value):
+        self._author = value
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+    @property
+    def genre(self):
+        return self._genre
+
+    @genre.setter
+    def genre(self, value):
+        self._genre = value
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
